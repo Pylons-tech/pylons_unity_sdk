f368fbfa43b876b6e904d81a45046688773ad800
sine/cosine/tangent <zfshmn@gmail.com>
2020-12-14 01:12:29 -0800

    Start sketching out RecipeBuilder datatypes + editors
    We're gonna need to move very rapidly w/ RecipeBuilder to make the demo video work, so sketching out the way it handles and edits data ASAP seems wise. This is just a bunch of empty .cs files, but they're the empty .cs files we need for our data flow and the editors associated with it.

---
+++ PylonsSdk/RecipeBuilder/Scripts/OutputTableSpec.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PylonsSdk.RecipeBuilder
+{
+    public class OutputTableSpec : ScriptableObject
+    {
+        public string Name;
+        public string Description;
+        public double Weight;
+        public CoinOutputSpec[] CoinOutputs;
+        public ItemInputTransformationSpec[] ItemInputTransformations;
+        public ItemOutputSpec[] ItemOutputs;
+    }
+}
---
+++ PylonsSdk/RecipeBuilder/Scripts/RecipeSpec.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PylonsSdk.RecipeBuilder
+{
+    [CreateAssetMenu(fileName = "New Recipe", menuName = "Pylons/Recipe")]
+    public class RecipeSpec : ScriptableObject
+    {
+        public CookbookSpec Cookbook;
+        public string Name;
+        public string Description;
+        public bool Enabled;
+        public TimeSpan ExecutionDelay;
+        public CoinInputSpec[] CoinInputs;
+        public ItemInputSpec[] ItemInputs;
+        // how do we manage public/local node stuff here? (we don't need to actually do it, but we need to know what the ui looks like and how the data is stored)
+    }
+}
